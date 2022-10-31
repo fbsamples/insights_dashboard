acc9e65a253236c521bd3b4382c016c2d5868fbc
Laura de Aguiar Dalmolin <lauraadalmolin@fb.com>
2022-10-31 10:51:39 -0300

    migrate static insights information to separate file

--- pages/page-insights.js
+++ pages/page-insights.js
@@ -1,91 +1,31 @@
 import Section from '../components/section';
 import DashboardChart from '../components/chart';
-import styles from '../styles/styles.module.css'
+import styles from '../styles/styles.module.css';
+
+import pageInsights from '../constants/page-insights.json';
 
 const PageInsights = () => {
 
-  const impressionsData = [
-
-    {
-      id: 1,
-      link:'https://developers.facebook.com/docs/graph-api/reference/v14.0/insights#tab-types',
-      linkLabel: 'Please check the documentation for more details',
-      apiName: 'page-insights',
-      metric: 'page_views_total',
-      type: 'bar',
-    },
-     {
-      id: 2,
-      link:'https://developers.facebook.com/docs/graph-api/reference/v14.0/insights#tab-types',
-      linkLabel: 'Please check the documentation for more details',
-      apiName: 'page-insights',
-      metric: 'page_consumptions',
-      type: 'line',
-    },
-     {
-      id: 3,
-      link:'https://developers.facebook.com/docs/graph-api/reference/v14.0/insights#tab-types',
-      linkLabel: 'Please check the documentation for more details',
-      apiName: 'page-insights',
-      metric: 'page_impressions,page_impressions_unique,page_impressions_paid,page_impressions_viral,page_impressions_organic',
-      type: 'line',
-    },
-  ]
-
-    const peopleInsights = [
-      {
-        id: 4,
-        link:'https://developers.facebook.com/docs/graph-api/reference/v14.0/insights#tab-types',
-        linkLabel: 'Please check the documentation for more details',
-        apiName: 'page-insights',
-        metric: 'page_engaged_users',
-        type: 'bar',
-      },
-      {
-        id: 5,
-        link:'https://developers.facebook.com/docs/graph-api/reference/v14.0/insights#tab-types',
-        linkLabel: 'Please check the documentation for more details',
-        apiName: 'page-insights',
-        metric: 'page_fans',
-        type: 'single_number',
-      },
-    ];
-
   return <div>
-
-    <Section title='People'>
-      <div className={styles.rowContainer}>
-        { peopleInsights.map(el => {
-            return <DashboardChart
-              key={el.id.toString()}
-              size={el.size}
-              link={el.link}
-              linkLabel={el.linkLabel}
-              apiName={el.apiName}
-              type={el.type}
-              metric={el.metric}>
-            </DashboardChart>
-          })
-        }
-      </div>
-    </Section>
-    <Section title='Page Impressions'>
-      <div className={styles.rowContainer}>
-        { impressionsData.map(el => {
-            return <DashboardChart
-              key={el.id.toString()}
-              size={el.size}
-              link={el.link}
-              linkLabel={el.linkLabel}
-              apiName={el.apiName}
-              type={el.type}
-              metric={el.metric}>
-            </DashboardChart>
-          })
-        }
-      </div>
-    </Section>
-
+    { pageInsights.sections.map(section => {
+        return <Section title={section.title} key={section.title}>
+          <div className={styles.rowContainer}>
+            { section.metrics.map(el => {
+                return <DashboardChart
+                  key={el.id.toString()}
+                  size={el.size}
+                  link={el.link}
+                  linkLabel={el.linkLabel}
+                  apiName={el.apiName}
+                  type={el.type}
+                  metric={el.metric}>
+                </DashboardChart>
+              })
+            }
+          </div>
+        </Section>
+      })
+    }
   </div>;
 }
 
