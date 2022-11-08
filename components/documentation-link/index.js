20d0810d922c66289b437bbd8953085ebce6fdfb
Laura de Aguiar Dalmolin <lauraadalmolin@fb.com>
2022-11-08 12:36:13 -0300

    create documentation-link component

---
+++ components/documentation-link/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BsFillBookFill } from "react-icons/bs";
+
+import styles from './style.module.css';
+
+const DocumentationLink = ({ description, linkLabel, link }) => {
+    return <div className={styles.container}>
+        <BsFillBookFill className={styles.icon}/>
+        { description }&nbsp;
+        <a className={styles.link} href={link} target="blank">{linkLabel}</a>
+    </div>
+}
+
+export default DocumentationLink;
