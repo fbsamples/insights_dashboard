7880ab614ea1c43a22ea3a58c1d967532c8f25b1
Laura de Aguiar Dalmolin <lauraadalmolin@fb.com>
2022-11-21 16:29:47 -0300

    add setting to add shadow to card dynamically

--- components/card/index.js
+++ components/card/index.js
@@ -2,7 +2,7 @@ import styles from './style.module.css'
 
 const Card = (props) => {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${props.shadow ? styles.shadow : styles.outline}`}>
       {props.children}
     </div>
   );
