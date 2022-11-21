0bc8ed1194090edee95312ba7e689b3344baefce
Laura de Aguiar Dalmolin <lauraadalmolin@fb.com>
2022-11-21 16:36:44 -0300

    update reel insights page

--- pages/reels-insights.js
+++ pages/reels-insights.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import Section from '../components/section';
+import DashboardChart from '../components/chart';
 import DocumentationLink from '../components/documentation-link';
+import VideoCard from '../components/video-card';
 import ReelInsights from '../components/reel-insights';
-import styles from '../styles/styles.module.css';
+import styles from '../styles/style.module.css';
 
 import settings from '../constants/settings.json';
 import reelsInsights from '../constants/reels-insights.json';
 
 const ReelsInsights = () => {
   const [reels, setReels] = useState([]);
-  const { metric, apiName, id } = reelsInsights.sections[0].metrics[0];
+  const section = reelsInsights.sections[0];
 
-  const getInsights = async (metric) => {
+  const getReels = async (signal) => {
     try {
       const url = `${settings.backendUrl}/api/get-reels-posts`;
-      const res = await fetch(url);
+      const res = await fetch(url, { signal });
       const { data, error } = await res.json();
 
       if (data) {
-        setReels([data[0]]);
+        setReels(data);
       } else {
         setErrorMessage(error.message.substr(6));
       }
@@ -29,30 +31,53 @@ const ReelsInsights = () => {
   };
 
   useEffect(() => {
-    getInsights(metric);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    getReels(signal);
+    return () => controller.abort();
   }, []);
+
+  const renderReelInsights = (video) => {
+    return <VideoCard key={video.id} video={video}>
+       { section.metrics.map(el => {
+            return renderChart(el, video.id);
+          })
+        }
+    </VideoCard>
+  }
+
+  const renderChart = (el, videoId) => {
+    return <DashboardChart
+      key={el.id.toString()}
+      size={el.size}
+      link={el.link}
+      linkLabel={el.linkLabel}
+      apiName={el.apiName}
+      type={el.type}
+      metric={el.metric}
+      title={el.title}
+      description={el.description}
+      icons={el.icons}
+      labels={el.labels}
+      plural={el.plural}
+      wrapMetricName={el.wrapMetricName}
+      videoId={videoId}>
+    </DashboardChart>;
+  }
 
   return <div>
     <DocumentationLink
       description={reelsInsights.docs.description}
       link={reelsInsights.docs.link}
       linkLabel={reelsInsights.docs.linkLabel}/>
-      { reelsInsights.sections.map(section => {
-        return <Section title={section.title} key={section.title}>
-          <div className={styles.rowContainer}>
-            { reels.map(reel => {
-                return <ReelInsights
-                  key={reel.id}
-                  metric={metric}
-                  apiName={apiName}
-                  reelId={reel.id}
-                  reelDescription={reel.description}/>
-              })
-            }
-          </div>
-        </Section>
-        })
-      }
+    <Section title={section.title} key={section.title}>
+      <div className={styles.rowContainer}>
+        { reels.map(reel => {
+            return renderReelInsights(reel);
+          })
+        }
+      </div>
+    </Section>
   </div>;
 }
 
