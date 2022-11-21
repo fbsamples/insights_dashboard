e6533d97aabd2076a4663849ebadea0c4992416c
Laura de Aguiar Dalmolin <lauraadalmolin@fb.com>
2022-11-21 16:31:49 -0300

    create component to display two single number metrics simultaneously

---
+++ components/double-number/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import * as AntDesignIcons from "react-icons/ai";
+
+import { abbreviateNumber } from '../../utils/strings';
+
+import styles from './style.module.css';
+
+const DoubleNumber = ({ datasets, icons, labels }) => {
+
+    const getIcon = (label) => {
+        return AntDesignIcons[icons[label]];
+    }
+
+    const getLabel = (label) => {
+        return labels[label];
+    }
+
+    const getLastNumber = (data) => {
+        const len = data.length;
+        const lastNumber = data[len - 1];
+        return abbreviateNumber(lastNumber);
+    }
+
+    return <div>
+        <p className={styles.metricContainer}>
+            { getLastNumber(datasets[0].data) } { getLabel(datasets[0].label) }
+        </p>
+        { datasets.length > 1 && <p className={styles.secondMetricContainer}>
+            { getLastNumber(datasets[1].data) } { getLabel(datasets[1].label) }
+        </p> }
+
+    </div>;
+}
+
+export default DoubleNumber;
