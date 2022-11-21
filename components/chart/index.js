24c98be46e887b06593cf7930e236eb4b1c28660
Laura de Aguiar Dalmolin <lauraadalmolin@fb.com>
2022-11-21 16:41:00 -0300

    add support to pie chart and double-number component

--- components/chart/index.js
+++ components/chart/index.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-import * as AntDesignIcons from "react-icons/ai";
-import { Line, Bar } from "react-chartjs-2";
+import { Line, Bar, Pie } from "react-chartjs-2";
 
 import AggregateByProperty from '../aggregate-by-property';
 import Card from '../card';
 import Chart from "chart.js/auto";
 import SingleNumber from '../single-number';
+import DoubleNumber from '../double-number';
 import ErrorCard from '../error-card';
 import Tooltip from '../tooltip';
 
 import { capitalizeAll } from '../../utils/strings';
 import { formatTimestampToDate, getLast30DaysInterval } from '../../utils/date';
-import { generateRandomColor } from '../../utils/color';
+import { generateRandomColor, generateRandomColorArray } from '../../utils/color';
 
+import constants from '../../constants/constants.json';
 import settings from '../../constants/settings.json';
 import styles from './style.module.css';
 
-const DashboardChart = ({ link, linkLabel, type, metric, apiName, icons, title, description, period='day' }) => {
+const DashboardChart = ({ link, linkLabel, type, metric, apiName, icons, title, description, videoId, labels, plural, wrapMetricName, period='day' }) => {
   const { since, until } = getLast30DaysInterval();
 
   const [isShown, setIsShown] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [tooltipInfo, setToolTipInfo] = useState([]);
   const [chartData, setChartData] = useState({
     labels: [],
     datasets: [
@@ -58,8 +60,7 @@ const DashboardChart = ({ link, linkLabel, type, metric, apiName, icons, title, 
     for (let i = 0; i < size; i++) {
 
       const el = metric.values[i];
-
-      if (Number.isInteger(el.value)) {
+      if (!isNaN(el.value)) {
         const standardSeriesName = 'value';
         if (!seriesMap.has(standardSeriesName)) {
           const dataset = createDefaultDataset(metric.name, size);
@@ -81,50 +82,74 @@ const DashboardChart = ({ link, linkLabel, type, metric, apiName, icons, title, 
   }
 
   const getLabels = (data) => {
-    return data.values.map(({ end_time }) => {
-      return formatTimestampToDate(end_time);
-    });
+    return data.values.map((value)  => {
+      if (value.end_time) return formatTimestampToDate(value.end_time);
+      if (Object.keys(value.value).length > 0) {
+        return [...Object.keys(value.value)];
+      }
+      return data.name;
+    }).flat();
+  }
+
+  const formatDataForPieChart = (datasets) => {
+      const completeDataset = createDefaultDataset('default', 0);
+
+      for (const dataset of datasets) {
+        completeDataset.data.push(dataset.data[0]);
+      }
+
+      delete(completeDataset.borderColor);
+      completeDataset.backgroundColor = generateRandomColorArray(completeDataset.data.length);
+      completeDataset.loaded = true;
+
+      return completeDataset;
   }
 
   const dataChangedHandler = (apiData) => {
 
-    const datasets = [];
+    let datasets = [];
     for (const metric of apiData) {
       const series = createSeriesMap(metric);
-
       for (const [seriesName, dataset] of series) {
         dataset.loaded = true;
         datasets.push(dataset);
       }
     }
 
-    const firstSeries = apiData[0];
-    const labels = getLabels(firstSeries);
+    if (type === 'pie') {
+      const newDataset = formatDataForPieChart(datasets);
+      datasets = [];
+      datasets.push(newDataset);
+    }
 
+    const firstSeries = apiData[0];
+    const chartLabels = getLabels(firstSeries);
+
+    setToolTipInfo(apiData);
     setChartData((curInputValues) => {
       return {
         ...curInputValues,
         datasets: datasets,
-        labels: labels,
+        labels: chartLabels,
         title: title || firstSeries.title,
-        name: firstSeries.name,
-        description: description || firstSeries.description
+        name: metric,
+        description: description || firstSeries.description,
       };
     });
 
   }
 
-  const getInsightsMetrics = async (metric) => {
+  const getInsightsMetrics = async (metric, signal) => {
     try {
       const url = `${settings.backendUrl}/api/${apiName}`;
-      const body = JSON.stringify({ metric, since, until, period });
-      const res = await fetch(url, { method: 'POST', body });
-      const { data, error } = await res.json();
+      const body = JSON.stringify({ metric, since, until, period, videoId });
+      const res = await fetch(url, { method: 'POST', body, signal });
+      const response = await res.json();
 
-      if (data) {
-        dataChangedHandler(data);
+      if (response.data && response.data.length > 0) {
+        dataChangedHandler(response.data);
       } else {
-        setErrorMessage(error.message.substr(6));
+        setErrorMessage(response.error.message.substr(6));
       }
 
     } catch (err) {
@@ -133,7 +158,10 @@ const DashboardChart = ({ link, linkLabel, type, metric, apiName, icons, title, 
   };
 
   useEffect(() => {
-    getInsightsMetrics(metric);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    getInsightsMetrics(metric, signal);
+    return () => controller.abort();
   }, []);
 
   return (
@@ -141,25 +169,22 @@ const DashboardChart = ({ link, linkLabel, type, metric, apiName, icons, title, 
       className={styles.mainDiv}
       onMouseEnter={() => setIsShown(true)}
       onMouseLeave={() => setIsShown(false)}>
-          { chartData.datasets[0].loaded && <Card>
+          { chartData.datasets.length > 0 && chartData.datasets[0].loaded && <Card shadow={apiName !== 'video-insights'}>
 
             <h1 className={styles.chartTitle}> { capitalizeAll(chartData.title) }</h1>
-            <p className={styles.metricName}> ({ chartData.name }) </p>
+            <div className={`${styles.metricName} ${wrapMetricName ? styles.wrap : ''}`} title={chartData.name}> ({ chartData.name }) </div>
 
             { type === 'line' && <Line data={chartData} /> }
-            { type === 'bar' && <Bar data={chartData} /> }
-            { type === 'single_number' && <SingleNumber data={chartData} icons={icons} /> }
-            { type === 'aggregate_by_property' && <AggregateByProperty datasets={chartData.datasets} icons={icons} /> }
+            { type === 'bar' && <Bar data={chartData} options={settings.barChart} /> }
+            { type === 'pie' && <Pie data={chartData} options={settings.pieChart} height={250} width={400}/> }
+            { type === 'single_number' && <SingleNumber data={chartData} icons={icons} labels={labels}/> }
+            { type === 'double_number' && <DoubleNumber datasets={chartData.datasets} icons={icons} labels={labels}/> }
+            { type === 'aggregate_by_property' && <AggregateByProperty datasets={chartData.datasets} plural={plural} icons={icons} /> }
 
-            { isShown && type !== 'single_number' && <Tooltip
-                title={title || chartData.name}
-                description={chartData.description}
-                link={link}
-                linkLabel={linkLabel}/>
-            }
+            { isShown && type !== 'single_number' && <Tooltip info={tooltipInfo}/> }
           </Card>
         }
-        { errorMessage && <ErrorCard icon="AiFillInfoCircle"> {errorMessage}</ErrorCard> }
+        { errorMessage && <ErrorCard icon="AiFillInfoCircle"> {errorMessage} </ErrorCard> }
     </div>
   );
 }
